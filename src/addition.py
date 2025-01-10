564230f3b8692c4ad9fd89c8a5b5900da7a9c414
iamvikramkumar <iamvikramkumar5@gmail.com>
2025-01-10 14:09:06 +0530

    update-function

--- src/addition.py
+++ src/addition.py
@@ -3,6 +3,16 @@
 def add(a, b):
     return a + b
 
+def mul(a, b):
+    return a * b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+
+# use add function and print result
+print(add(1, 2))
+
+# use mul function and print result
+print(mul(1, 2))
